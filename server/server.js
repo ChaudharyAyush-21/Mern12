61aee5211c4de5893f2a90be27326513004c03e1
ChaudharyAyush-21 <ayushk2194@gmail.com>
2025-07-26 17:29:40 +0530

    adding edit functionality in frontend

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000;
 
 const corsoptions = {
     origin:"http://localhost:5173",
-    methods: "GET, POST, PUT, DELETE",
+    methods: "GET, POST, PUT, DELETE, PATCH",
     credentials: true,
 }
 
