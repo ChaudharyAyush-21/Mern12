4727879e6e1bdaf83addd3e2570828d9515a1658
ChaudharyAyush-21 <ayushk2194@gmail.com>
2025-07-26 19:04:42 +0530

    adding the secure path to admin pannel

--- client/src/components/Admin-Layout.jsx
+++ client/src/components/Admin-Layout.jsx
@@ -1,8 +1,20 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Navigate, NavLink, Outlet } from 'react-router-dom';
 import { FaUser , FaRegListAlt , FaHome} from "react-icons/fa";
 import { FaMessage } from "react-icons/fa6";
+import { useAuth } from '../store/auth';
 
 const AdminLayout = () => {
+
+    const { user,isLoading } = useAuth();
+
+    if(isLoading){
+        return <h1>Loading...</h1>
+    }
+
+    if (!user.isAdmin){
+        return <Navigate to="/" />
+    }
+
   return (
     <>
         <header>
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -9,8 +9,8 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
 
     const [token , setToken] = useState(localStorage.getItem('token'));
-
     const [user , setUser] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
     const [services, setServices] = useState("");
     const authorizationToken = `Bearer ${token}`;
 
@@ -29,6 +29,7 @@ export const AuthProvider = ({ children }) => {
     //Authentication for the user data
     const userAuthentication = async () => {
         try {
+            setIsLoading(true);
             const response = await fetch(`http://localhost:4000/api/auth/user`, {
                 method: 'GET',
                 headers: {
@@ -39,9 +40,11 @@ export const AuthProvider = ({ children }) => {
                 const data = await response.json();
                 console.log("User data:", data.userData);
                 setUser(data.userData);
+                setIsLoading(false);
                 
             } else {
                 console.error("Failed to fetch user data");
+                setIsLoading(false);
             }
         } catch (error) {
             console.error("Error fetching user data:", error);
@@ -72,7 +75,7 @@ export const AuthProvider = ({ children }) => {
 
     // This is where you would typically manage authentication state
     return (
-        <AuthContext.Provider value={{ isLoggedIn, storetokenInLS , LogoutUser , user ,services , authorizationToken}}>
+        <AuthContext.Provider value={{ isLoggedIn, storetokenInLS , LogoutUser , user ,services , authorizationToken , isLoading }}>
         {children}
         </AuthContext.Provider>
     );
